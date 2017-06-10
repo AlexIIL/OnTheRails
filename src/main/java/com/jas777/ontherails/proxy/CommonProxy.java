cd9882ede94049306808bfa0a9dde8c88223240b
jas777_PL <jas.czeszejko@wp.pl>
2017-06-10 23:11:13 +0200

    Project

---
+++ src/main/java/com/jas777/ontherails/proxy/CommonProxy.java
@@ -0,0 +1,5 @@
+package com.jas777.ontherails.proxy;
+
+public interface CommonProxy {
+
+}
---
+++ src/main/java/com/jas777/ontherails/proxy/ServerProxy.java
@@ -0,0 +1,5 @@
+package com.jas777.ontherails.proxy;
+
+public class ServerProxy implements com.jas777.ontherails.proxy.CommonProxy{
+
+}
